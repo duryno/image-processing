f37af540ef419e65888b8bb41ed38cc06ac8ba24
Juraj Orszag <orszag.juraj@gmail.com>
2018-01-18 10:45:03 +0100

    Revised algorithm for re-submission Added README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# image-processing
+## Results for N=11
+### Tsukuba
+![alt text](images/test-tsuka11-18-2.png)
+
+### Venus
+![alt text](images/venus11-18.png)
+
+### Map
+![alt text](images/map11-18.png)
--- assignment3.py
+++ assignment3.py
@@ -1,30 +1,32 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-from scipy import misc, ndimage
+from scipy import misc
 
 # descriptor size
-n = 5
+n = 11
 
 # load images
-template = cv2.imread("tsukuba/scene1.row3.col1.ppm", cv2.IMREAD_GRAYSCALE)
-img = cv2.imread("tsukuba/scene1.row3.col2.ppm", cv2.IMREAD_GRAYSCALE)
+# template = cv2.imread("tsukuba/scene1.row3.col1.ppm", cv2.IMREAD_GRAYSCALE)
+# img = cv2.imread("tsukuba/scene1.row3.col2.ppm", cv2.IMREAD_GRAYSCALE)
+template = cv2.imread("venus/im0.ppm", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("venus/im6.ppm", cv2.IMREAD_GRAYSCALE)
+# template = cv2.imread("map/im0.pgm", cv2.IMREAD_GRAYSCALE)
+# img = cv2.imread("map/im1.pgm", cv2.IMREAD_GRAYSCALE)
 
 def getDescriptors(img, y, n):
     descriptors = []
-    for x in range(n, len(img[y])-n):
+    for x in range(n//2, len(img[y])-n//2):
         try:
             nd = []
-            for i in range(int(x)-(n//2), int(x)+(n//2)+1):
-                for j in range(int(y)-(n//2), int(y)+(n//2)+1):
+            for i in range(int(x)-(n//2), int(x)+(n//2)):
+                for j in range(int(y)-(n//2), int(y)+(n//2)):
                     nd.append(img[j,i])
             descriptors.append(nd)
         except IndexError :
-            print(str(x))
             print("index error")
     return descriptors
 
-# create pyramide
+# create pyramid
 def buildPyramid(img):
     pyramide = []
     pyramide.append(img)
@@ -33,85 +35,52 @@ def buildPyramid(img):
         pyramide.append(low_res)
     return pyramide
 
-def getLocalMean(x, y, width, height, scale):
-    print(x)
-    print(width)
-
+def getLocalMean(x, y, scale):
     dis = []
-    if((int(x)-(n//2)) >= 0):
-        safeMinX = int(x)-(n//2)
-    else:
-        safeMinX = 0
-
-    if((int(x)+(n//2)+1) < width):
-        safeMaxX = int(x)+(n//2)+1
-    else:
-        safeMaxX = width
-
-    if((int(y)-(n//2)) >= 0):
-        safeMinY = int(y)-(n//2)
-    else:
-        safeMinY = 0
-
-    if((int(y)+(n//2)+1) < height):
-        safeMaxY = int(y)+(n//2)+1
-    else:
-        safeMaxY = height
-
-    for i in range(int(safeMinX), int(safeMaxX)):
-        for j in range(int(safeMinY), int(safeMaxY)):
+    for i in range(x-n//2, x+n//2):
+        for j in range(y-n//2, y+n//2):
             row = int(i/scale)
             col = int(j/scale)
             dis.append(disparities[row,col])
     return np.mean(dis)
 
+# first imagae
 p1 = buildPyramid(img)
 p2 = buildPyramid(template)
 height, width = p1[3].shape
-correlations = np.zeros((height, width))
 disparities = np.zeros((height, width))
 depth = np.zeros((height, width))
-for l in range(n, height-n):
+for l in range(0, height-n//2):
     des = getDescriptors(p1[3], l, n)
     des2 = getDescriptors(p2[3], l, n)
     d1 = np.asarray(des)
     d2 = np.asarray(des2)
-    column = n
+    column = n//2
     for index, descriptor in enumerate(d1):
-        disparities[l, column] = width
-        row_corrs = []
-        smallest_corr = 1
+        highest_corr = -1
         for index2, descriptor2 in enumerate(d2):
-            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_SQDIFF_NORMED)
-            row_corrs.append(result)
+            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_CCORR_NORMED)
 
-            if(result < smallest_corr):
-                smallest_corr = result
+            if(result > highest_corr):
+                highest_corr = result
                 disparities[l, column] = (index2 - index)
                 depth[l, column] = (index2 - index) *  (1.0 / 16.0)
         if(depth[l, column] == 0.0):
-            disparities[l, column] = getLocalMean(l, column, width, height, 1)
-            depth[l, column] = 2 * (1.0 / 16.0)
-        correlations[l, column] = min(row_corrs)
+            disparities[l, column] = getLocalMean(l, column, 1)
+            depth[l, column] = disparities[l,column] * (1.0 / 16.0)
         column += 1
 
-cv2.imshow('test3', depth)
-pic = depth * 255
-pic = pic.astype('uint8')
-cv2.imwrite('test3.png', pic)
-
+# second image
 height, width = p1[2].shape
-correlations = np.zeros((height, width))
 depth = np.zeros((height, width))
-for l in range(n, height-n):
+for l in range(0, height-n//2):
     des = getDescriptors(p1[2], l, n)
     des2 = getDescriptors(p2[2], l, n)
     d1 = np.asarray(des)
     d2 = np.asarray(des2)
-    column = n
+    column = n//2
     for index, descriptor in enumerate(d1):
-        row_corrs = []
-        smallest_corr = 1
+        highest_corr = -1
         int_disparities = disparities.astype(int)
         row = int(l/2)
         col = int(column/2)
@@ -119,38 +88,30 @@ for l in range(n, height-n):
             i = k
             if(i<0):
                 i = 0
-            elif(i > width - 2*n - 1):
-                i = width - 2*n - 1
+            elif(i > width - n):
+                i = width - n
             descriptor2 = d2[i]
-            index2 = k
-            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_SQDIFF_NORMED)
-            row_corrs.append(result)
-            if(result < smallest_corr):
-                smallest_corr = result
-                disparities[row, col] = (index2 - index)
-                depth[l, column] = (index2 - index) *  (1.0 / 16.0)
+            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_CCORR_NORMED)
+            if(result > highest_corr):
+                highest_corr = result
+                disparities[row, col] = (k - index)
+                depth[l, column] = (k - index) *  (1.0 / 16.0)
         if(depth[l, column] == 0.0):
-            disparities[row, col] = getLocalMean(l, column, width, height, 2)
-            depth[l, column] = 2 * (1.0 / 16.0)
+            disparities[row, col] = getLocalMean(l, column, 2)
+            depth[l, column] = disparities[row, col] * (1.0 / 16.0)
         column += 1
 
-cv2.imshow('test2', depth)
-pic = depth * 255
-pic = pic.astype('uint8')
-cv2.imwrite('test2.png', pic)
-
+# third image
 height, width = p1[1].shape
-correlations = np.zeros((height, width))
 depth = np.zeros((height, width))
-for l in range(n, height-n):
+for l in range(0, height-n//2):
     des = getDescriptors(p1[1], l, n)
     des2 = getDescriptors(p2[1], l, n)
     d1 = np.asarray(des)
     d2 = np.asarray(des2)
-    column = n
+    column = n//2
     for index, descriptor in enumerate(d1):
-        row_corrs = []
-        smallest_corr = 1
+        highest_corr = -1
         int_disparities = disparities.astype(int)
         row = int(l/4)
         col = int(column/4)
@@ -158,38 +119,30 @@ for l in range(n, height-n):
             i = k
             if(i<0):
                 i = 0
-            elif(i > width - 2*n - 1):
-                i = width - 2*n - 1
+            elif(i > width - n):
+                i =  width - n
             descriptor2 = d2[i]
-            index2 = k
-            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_SQDIFF_NORMED)
-            row_corrs.append(result)
-            if(result < smallest_corr):
-                smallest_corr = result
-                disparities[row, col] = (index2 - index)
-                depth[l, column] = (index2 - index) *  (1.0 / 16.0)
+            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_CCORR_NORMED)
+            if(result > highest_corr):
+                highest_corr = result
+                disparities[row, col] = (k - index)
+                depth[l, column] = (k - index) *  (1.0 / 16.0)
         if(depth[l, column] == 0.0):
-            disparities[row, col] = getLocalMean(l, column, width, height, 4)
-            depth[l, column] = 2 * (1.0 / 16.0)
+            disparities[row, col] = getLocalMean(l, column, 4)
+            depth[l, column] = disparities[row, col] * (1.0 / 16.0)
         column += 1
 
-cv2.imshow('test1', depth)
-pic = depth * 255
-pic = pic.astype('uint8')
-cv2.imwrite('test1.png', pic)
-
+# final image
 height, width = p1[0].shape
-correlations = np.zeros((height, width))
 depth = np.zeros((height, width))
-for l in range(n, height-n):
+for l in range(0, height-n//2):
     des = getDescriptors(p1[0], l, n)
     des2 = getDescriptors(p2[0], l, n)
     d1 = np.asarray(des)
     d2 = np.asarray(des2)
-    column = n
+    column = n//2
     for index, descriptor in enumerate(d1):
-        row_corrs = []
-        smallest_corr = 1
+        highest_corr = -1
         int_disparities = disparities.astype(int)
         row = int(l/8)
         col = int(column/8)
@@ -197,26 +150,25 @@ for l in range(n, height-n):
             i = k
             if(i<0):
                 i = 0
-            elif(i > width - 2*n - 1):
-                i = width - 2*n - 1
+            elif(i > width - n):
+                i = width - n
             descriptor2 = d2[i]
-            index2 = k
-            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_SQDIFF_NORMED)
-            row_corrs.append(result)
+            result = cv2.matchTemplate(descriptor, descriptor2, cv2.TM_CCORR_NORMED)
 
-            if(result < smallest_corr):
-                smallest_corr = result
-                disparities[row, col] = (index2 - index)
-                depth[l, column] = (index2 - index) *  (1.0 / 16.0)
+            if(result > highest_corr):
+                highest_corr = result
+                disparities[row, col] = (k - index)
+                depth[l, column] = (k - index) *  (1.0 / 16.0)
+        # ensure smoothness
         if(depth[l, column] == 0.0):
-            disparities[row, col] = getLocalMean(l, column, width, height, 8)
-            depth[l, column] = 2 * (1.0 / 16.0)
+            disparities[row, col] = getLocalMean(l, column, 8)
+            depth[l, column] = disparities[row, col] * (1.0 / 16.0)
         column += 1
 
 cv2.imshow('test', depth)
-pic = depth * 255
-pic = pic.astype('uint8')
-cv2.imwrite('test.png', pic)
+# pic = depth * 255
+# pic = pic.astype('uint8')
+# cv2.imwrite('test-venus11-18.png', pic)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
